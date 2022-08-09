7a706c01b88e3a216122bdf635f2be1c3f20248d
Sumitra <skhadka@qkly.io>
2022-08-09 08:04:43 +0545

    Add global css and import all component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/About'
+import Experience from './components/experience/experience'
+import MyServices from './components/myservices/myservices'
+import Portfolio from './components/portfolio/portfolio'
+import Testimonial from './components/testimonials/testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/footer'
 
 const App = () => {
   return (
     <>
         <Header/>
+        <Nav/>
+        <About/>
+        <Experience/>
+        <MyServices/>
+        <Portfolio/>
+        <Testimonial/>
+        <Contact/>
+        <Footer/>
+        
     </>
   )
 }
---
+++ src/components/myservices/myservices.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './myservices.css'
+
+const myservices = () => {
+  return (
+    <div>myservices</div>
+  )
+}
+
+export default myservices
