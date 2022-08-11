9490492a1a78ca109b6d760be80666957d8ae6fc
Sumitra <skhadka@qkly.io>
2022-08-11 21:08:34 +0545

    Added my services UI and portfolio image

--- src/components/myservices/myservices.jsx
+++ src/components/myservices/myservices.jsx
@@ -1,9 +1,108 @@
 import React from 'react'
 import './myservices.css'
+import {BiCheck} from 'react-icons/bi'
 
 const myservices = () => {
   return (
-    <section id='services'></section>
+    <section id='services'>
+        <h5>What I Offer</h5>
+        <h2>Services</h2>
+        <div className='container services__container'>
+            <article className='service'>
+                <div className='service__head'>
+                    <h3>UI/UX Design</h3>
+                </div>
+                <ul className='service__list'>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                </ul>
+            </article>
+            <article className='service'>
+                <div className='service__head'>
+                    <h3>Web Development</h3>
+                </div>
+                <ul className='service__list'>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                </ul>
+            </article>
+            <article className='service'>
+                <div className='service__head'>
+                    <h3>Page Speed Optimization</h3>
+                </div>
+                <ul className='service__list'>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon'/>
+                        <p>Page Speed Optimization</p>
+                    </li>
+                </ul>
+            </article>
+        </div>
+    </section>
 
   )
 }
