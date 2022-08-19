5969c4e4a0069a680e6c3d6b831ab9b0bc1bb1cf
Sumitra <skhadka@qkly.io>
2022-08-19 10:59:13 +0545

    integrate form with email js

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
-import './contact.css'
+import React, { useRef } from 'react';
+import './contact.css';
+import { MdOutlineEmail } from 'react-icons/md';
+import { RiMessengerLine } from 'react-icons/ri';
+import { BsWhatsapp } from 'react-icons/bs';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
-  return (
-    <section id='contact'></section>
+    const form = useRef();
 
-  )
-}
+    const sendEmail = (e) => {
+        e.preventDefault();
 
-export default Contact
+        emailjs.sendForm('service_1bx1mfv', 'template_i9czpg1', form.current, '3CQuaSiLPEDCfP9PV').then(
+            (result) => {
+                console.log(result.text);
+            },
+            (error) => {
+                console.log(error.text);
+            }
+        );
+
+        e.target.reset();
+    };
+
+    return (
+        <section id='contact'>
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+            <div className='container contact__container'>
+                <div className='contact__options'>
+                    <article className='contact__option'>
+                        <MdOutlineEmail className='contact__option-icon' />
+                        <h4>Email</h4>
+                        <h5>sumitrakhadka37@gmail.com</h5>
+                        <a href='mailto:sumitrakhadka37@gmail.com' target='_blank'>
+                            Send a Message
+                        </a>
+                    </article>
+                    <article className='contact__option'>
+                        <RiMessengerLine className='contact__option-icon' />
+                        <h4>Messenger</h4>
+                        <h5>Sumitra Khadka</h5>
+                        <a href='https://m.me/sumitra.khadka.562' target='_blank'>
+                            Send a Message
+                        </a>
+                    </article>
+                    <article className='contact__option'>
+                        <BsWhatsapp className='contact__option-icon' />
+                        <h4>WhatsApp</h4>
+                        {/* <h4>dummt@gmail.com</h4> */}
+                        <a href='https://api.whatsapp.com/send?phone=9847623468' target='_blank'>
+                            Send a Message
+                        </a>
+                    </article>
+                </div>
+
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type='text' name='name' placeholder='Your Full Name' required></input>
+                    <input type='email' name='email' placeholder='Your Email' required></input>
+                    <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+                    <button type='submit' className='btn btn-primary'>
+                        Send Message
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
