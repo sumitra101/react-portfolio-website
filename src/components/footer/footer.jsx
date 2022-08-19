c152e3f9bcce2be7ed71c401425044f1a3e90bd3
Sumitra <skhadka@qkly.io>
2022-08-19 10:58:58 +0545

    add footer and contact section

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
-import './footer.css'
+import React from 'react';
+import './footer.css';
+import { FaFacebookF } from 'react-icons/fa';
+import { FiInstagram } from 'react-icons/fi';
+import { IoLogoTwitter } from 'react-icons/io';
 
 const footer = () => {
-  return (
-    <section id='#about'></section>
+    return (
+        <footer>
+            <a href='#' className='footer__logo'>
+                Sumitra
+            </a>
+            <ul className='permalinks'>
+                <li>
+                    <a href='#'>Home</a>
+                </li>
+                <li>
+                    <a href='#about'>About</a>
+                </li>
+                <li>
+                    <a href='#experience'>Experience</a>
+                </li>
+                <li>
+                    <a href='#services'>Services</a>
+                </li>
+                <li>
+                    <a href='#portfolio'>Portfolio</a>
+                </li>
+                <li>
+                    <a href='#testimonials'>Testimonials</a>
+                </li>
+                <li>
+                    <a href='#contact'>Contact</a>
+                </li>
+            </ul>
 
-  )
-}
+            <div className='footer__socials'>
+                <a href='https://facebook.com'>
+                    <FaFacebookF />
+                </a>
+                <a href='https://instagram.com'>
+                    <FiInstagram />
+                </a>
+                <a href='https://twitter.com'>
+                    <IoLogoTwitter />
+                </a>
+            </div>
 
-export default footer
+            <div className='footer__copyright'>
+                <small>&copy; Sumitra Khadka. All rights reserve</small>
+            </div>
+        </footer>
+    );
+};
+
+export default footer;
