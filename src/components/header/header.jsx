f186e3c1b557aade6961d0a497d8ec35b8c697c1
Sumitra <skhadka@qkly.io>
2022-08-25 11:24:43 +0545

    rename filename

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import ME from '../../assets/788af61d-662d-4caf-9d14-4487f3465e20-removebg-preview (Custom).png'
-import HeaderSocial from './headerSocials'
+import React from 'react';
+import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/788af61d-662d.png';
+import HeaderSocial from './headerSocials';
 
 const header = () => {
-  return (
-    <header>
-        <div className='container header__container'>
-            <h5>Hello I'm</h5>
-            <h1>Sumitra Khadka</h1>
-            <h5 className="text-light">Fullstack Developer</h5>
-            <CTA/>
-            <HeaderSocial/>
+    return (
+        <header>
+            <div className='container header__container'>
+                <h5>Hello I'm</h5>
+                <h1>Sumitra Khadka</h1>
+                <h5 className='text-light'>Fullstack Developer</h5>
+                <CTA />
+                <HeaderSocial />
 
-            <div className='me'>
-                 <img src={ME} alt="me"></img>
+                <div className='me'>
+                    <img src={ME} alt='me'></img>
+                </div>
+                <a href='#contact' className='scroll__down'>
+                    Scroll Down
+                </a>
             </div>
-        <a href='#contact' className='scroll__down'>Scroll Down</a>
-        </div>
-    </header>
-  )
-}
+        </header>
+    );
+};
 
-export default header
+export default header;
